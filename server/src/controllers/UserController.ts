0223edadb9f4c25d574774ab433195a0640d8002
kaushaljoshi53 <kjoshi.rkl@gmail.com>
2023-09-11 01:46:34 +0530

    initial work 2

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,18 +1,26 @@
+import { error } from "console";
 
+export function hasNullValues(obj:any):boolean{
 
+    for (const key in obj){
+        if(obj[key] === null){
+            return true;
+        }
+    }
+    return false;
+}
 
 
 export class Controller{
-    
+
+
     signin(req:Request, res:Response){
         
         const userData = req.body
         
-        if (!userData) {
-            
+        if (hasNullValues(userData)) {
+            res.status(400).json{{error: 'User data must not be null'}}
         }
-
-        return('asd')
     }
 
 }
